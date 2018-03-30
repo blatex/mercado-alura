d0500b586f9d2c800dff7fa746050f44b7a1fa90
Gabriel Blat <gblat7@gmail.com>
2018-03-29 23:37:26 -0300

    Capítulos 1-4

---
+++ application/views/produtos/index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="<?= base_url("css/bootstrap.css") ?>">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Produtos</h1>
+
+            <table class="table">
+                <?php foreach($produtos as $produto): ?>
+                <tr>
+                    <td><?= $produto["nome"] ?></td>
+                    <td><?= numeroEmReais($produto["preco"]) ?></td>
+                </tr>
+                <?php endforeach ?>
+            </table>
+        </div>
+    </body>
+</html>
